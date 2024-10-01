0f2fc5fe185b1f156b2b4948105769172d66bcae
Carlos Augusto Aquino <carlos.aquino@aluno.uece.br>
2024-10-01 11:14:37 -0300

    Add Files

---
+++ src/app/app.module.ts
@@ -0,0 +1,24 @@
+import { NgModule } from '@angular/core';
+import { BrowserModule } from '@angular/platform-browser';
+
+import { AppRoutingModule } from './app-routing.module';
+import { AppComponent } from './app.component';
+import { CabecalhoComponent } from './componentes/cabecalho/cabecalho.component';
+import { ContainerComponent } from './componentes/container/container.component';
+import { SeparadorComponent } from './componentes/separador/separador.component';
+
+@NgModule({
+  declarations: [
+    AppComponent,
+    CabecalhoComponent,
+    ContainerComponent,
+    SeparadorComponent
+  ],
+  imports: [
+    BrowserModule,
+    AppRoutingModule
+  ],
+  providers: [],
+  bootstrap: [AppComponent]
+})
+export class AppModule { }
